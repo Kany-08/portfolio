f89066e0bb73efaaaf7cbb617ba8fe7d7afd1510
Kanyshai Sharapova <kanysharapova@gmail.com>
2026-03-02 15:43:35 +0100

    Refactor Vite config to include build options and resolve paths for project inputs

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
-import {defineConfig} from "vite"
+import { resolve } from 'path'
+import { defineConfig } from "vite"
 
 export default defineConfig({
-	base: "./"
+	base: "./",
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'index.html'),
+				project1: resolve(__dirname, 'project-1.html'),
+				project2: resolve(__dirname, 'project-2.html'),
+				project3: resolve(__dirname, 'project-3.html'),
+			},
+		},
+	},
 })
